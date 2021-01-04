1f43fceb77d42d3f0be8f3530ca33d49faee0e4a
Jagadeesan Murugesan <jagadeesan.m@gmail.com>
2021-01-04 21:57:27 +0530

    Modified files:[main.tf]

--- gcp_compute_firewall_allow_ssh/main.tf
+++ gcp_compute_firewall_allow_ssh/main.tf
@@ -53,3 +53,5 @@ resource "google_compute_firewall" "default_firewall" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2021-01-04 21:57:27.300863]
